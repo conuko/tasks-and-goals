12613a193cbf6bf54a5ecdb87ba18f9d3e2a3474
Constantin <constantinu93@gmail.com>
2022-03-09 19:02:36 +0100

    feat: create first project structure

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello, this will be my "To Do and Goals App"</div>
+    </div>
+  );
+}
+
+export default App;
