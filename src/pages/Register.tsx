c3499fe1468c7b0f761e09ef59379aa776b27e10
Constantin <constantinu93@gmail.com>
2022-03-15 13:27:52 +0100

    feat: add Login and Register components

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
-const Login = () => {
+const Register = () => {
   const [userName, setUserName] = useState("John Smith");
 
   return (
     <div>
       <form>
+        <input id="name" name="name" type="text" />
+        <label htmlFor="name">Name</label>
         <input id="email" name="email" type="email" />
         <label htmlFor="email">E-Mail</label>
         <input id="password" name="password" type="password" />
@@ -15,4 +17,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
