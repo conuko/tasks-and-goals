7f614ecd252cdcbad8dd3d4a8be1d96f5211b737
conuko <constantinu93@gmail.com>
2022-04-22 10:53:47 +0200

    feat: add delete todo functionality

--- src/components/layout/TodoItem.tsx
+++ src/components/layout/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { VscClose } from "react-icons/vsc";
+import { useEffect } from "react";
 
 interface TodoItemProps {
   todo: any;
@@ -12,7 +13,32 @@ const TodoItem = (props: TodoItemProps) => {
   };
 
   const handleRemove = () => {
+    // function to remove todo from the server:
+    handleRemoveServer(props.todo.id);
+    // callback function to remove todo from the state:
     props.removeTodo(props.todo.id);
+  };
+
+  /*   const handleRemoveServer = (todo: any) => {
+    fetch(`http://localhost:5000/task/${todo}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }; */
+
+  const handleRemoveServer = async (todo: any) => {
+    try {
+      const response = await fetch(`http://localhost:5000/task/${todo}`, {
+        method: "DELETE",
+        headers: { "Content-type": "application/json" },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
