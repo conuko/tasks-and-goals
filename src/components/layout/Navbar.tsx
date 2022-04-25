ef499c3aaab9eb3996793faacbe8342edb14701a
conuko <constantinu93@gmail.com>
2022-04-25 11:14:41 +0200

    style: navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__element">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="navbar__element">
           <Link to="/todos">Todos</Link>
         </li>
       </ul>
