091faaade623845a1cebc4d2e5a75603ee4583d5
conuko <constantinu93@gmail.com>
2022-04-30 14:59:34 +0200

    feat: set up vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "@honkhonk/vite-plugin-svgr";
@@ -5,4 +8,9 @@ import svgr from "@honkhonk/vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
 });
