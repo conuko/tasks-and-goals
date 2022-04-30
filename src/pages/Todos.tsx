26221adf37d803750186d9745345f465ed639680
conuko <constantinu93@gmail.com>
2022-04-30 12:35:47 +0200

    refactor: put addtodo in todos component to fix trigger of useEffect

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,17 +1,63 @@
 import React, { useEffect, useState, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import TodoItem from "../components/layout/TodoItem";
-import AddTodo from "../components/layout/AddTodo";
 
 interface TodoProps {
   user: any;
 }
 
 const Todos = (props: TodoProps) => {
-  const [todos, setTodos] = React.useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [todos, setTodos] = React.useState<any[]>([]);
+  const [todo, setTodo] = useState({
+    id: "",
+    content: "",
+    checked: false,
+    authorEmail: "",
+  });
 
+  // form validation rules
+  const validationSchema = Yup.object().shape({
+    task: Yup.string().max(30, "A task cannot be more than 30 characters"),
+  });
+  const formOptions = { resolver: yupResolver(validationSchema) };
+
+  const { register, handleSubmit, setValue, reset, formState } =
+    useForm(formOptions);
+  const { errors } = formState;
+
+  const touchDirtyValidate = {
+    shouldTouch: true,
+    shouldDirty: true,
+    shouldValidate: true,
+  };
+
+  // change and submit handler for addtodo input form
+  const handleOnChange = (c: any) => {
+    setValue(c.target.name, c.target.value, touchDirtyValidate);
+    setTodo({ ...todo, content: c.target.value });
+  };
+
+  const handleOnSubmit = (e: any) => {
+    //const id = uuid();
+    const authorEmail = props.user.email;
+    if (todo.content.trim()) {
+      // function to add item to the server/database:
+      addTodoToServer({ ...todo, authorEmail });
+
+      // add item to the state:
+      setTodos([todo, ...todos]);
+      // sets the form text back to empty, after the item was added to the "items" state via the "addItem" callback function:
+      setTodo({ ...todo, content: "" });
+      setValue("task", "");
+    }
+  };
+
+  // fetch all todos from server everytime the todo state changes
   useEffect(() => {
-    setIsLoading(true);
+    todo.content === "" && setIsLoading(true);
     fetch(
       `https://shortlist-backend.herokuapp.com/tasks/author/${props.user.email}`,
       {
@@ -27,12 +73,22 @@ const Todos = (props: TodoProps) => {
         setTodos(data);
         setIsLoading(false);
       });
-  }, []);
+  }, [todo]);
 
-  // callback function to add todo to the todos state:
-  const addTodo = (todo: any) => {
-    setTodos([todo, ...todos]);
+  // add todo to the server
+  const addTodoToServer = (todo: any) => {
+    fetch("https://shortlist-backend.herokuapp.com/task/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   };
+
   // callback function to remove todo from the todos state:
   const removeTodo = (id: any) => {
     setTodos(todos.filter((todo) => todo.id !== id));
@@ -50,7 +106,23 @@ const Todos = (props: TodoProps) => {
   return (
     <div className="todos">
       <div className="flex-column">
-        <AddTodo addTodo={addTodo} user={props.user} />
+        <div className="flex-column">
+          <form onSubmit={handleSubmit(handleOnSubmit)}>
+            <input
+              {...register("task")}
+              className="input mr-medium"
+              type="text"
+              name="task"
+              id="task"
+              placeholder="Add a task"
+              onChange={(c) => handleOnChange(c)}
+            />
+            <button type="submit" disabled={todo.content === "" || errors.task}>
+              Add
+            </button>
+          </form>
+          <span>{errors.task?.message}</span>
+        </div>
         {isLoading ? (
           <div>Loading...</div>
         ) : (
